e3bbacbc62add68c36b2a6f9a22e27f24c534d47
andrii.smahin <andrii.smahin@masterofcode.com>
2021-11-04 18:21:11 +0200

    init

--- src/services/helpers/helper1.js
+++ src/services/helpers/helper1.js
@@ -1,10 +1,1 @@
-module.exports = (products, sortKey, sortValue) => {
-    if (!products) throw new Error("argument products does't exist");
-
-    return products.filter((product) => {
-        if (product[sortKey] && product[sortKey] === sortValue) {
-            return true;
-        }
-        return false;
-    });
-};
+module.exports = (products, sortKey, sortValue) => {};
--- src/services/helpers/helper3.js
+++ src/services/helpers/helper3.js
@@ -1,16 +1,5 @@
 function formatPriceToNumber(price) {
-    return price.replace('$', '').replace(',', '.');
+  return price.replace('$', '').replace(',', '.');
 }
 
-module.exports = (products) => {
-    if (!products) throw new Error("argument products does't exist");
-
-    return products.map((product) => ({
-        ...product,
-        price: Number(
-            product.pricePerKilo && product.weight
-                ? product.weight * formatPriceToNumber(product.pricePerKilo)
-                : product.quantity * formatPriceToNumber(product.pricePerItem),
-        ),
-    }));
-};
+module.exports = (products) => {};
