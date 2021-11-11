b329ec5c9a784f0905ff024bde95bd106a68d5e8
vkukur <vkukurika@essentialaccessibility.com>
2021-11-11 19:18:45 +0200

    Change requests

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const fetch = require('./fetch');
 
 const path = {
   posts: () => '/posts',
-  post: (id) => `/post/${id}`,
+  post: (id) => `/posts/${id}`,
   comments: (userId) => `/comments?userId=${userId}`,
 };
 
-fetch(path, (err, res) => {
-  if (err) console.error(err);
+console.log('make request to /posts');
 
-  console.log(res);
+fetch(path.posts(), (err, res) => {
+  if (err) console.error(err);
+  console.log('results were retrieved');
+
+  const postId = res[6].id;
+  fetch(path.post(postId), (postErr, postRes) => {
+    if (postErr) console.error(postErr);
+    console.log('post results were retrieved');
+
+    const userId = postRes.userId;
+
+    fetch(path.comments(userId), (commentsErr, commentsRes) => {
+      if (commentsErr) console.error(commentsErr);
+      console.log('comments results were retrieved');
+
+      console.log(commentsRes[1]);
+    });
+  });
 });
+
+console.log('after /posts request');
